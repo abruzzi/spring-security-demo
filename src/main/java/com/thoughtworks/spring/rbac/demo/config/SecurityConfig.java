711ca16c934cf09ac8f586616babd156920c5cf7
Qiu Juntao <juntao.qiu@gmail.com>
2016-05-06 00:01:24 +1000

    clean up, it works with fake-sso repo

--- src/main/java/com/thoughtworks/spring/rbac/demo/config/SecurityConfig.java
+++ src/main/java/com/thoughtworks/spring/rbac/demo/config/SecurityConfig.java
@@ -1,60 +1,35 @@
 package com.thoughtworks.spring.rbac.demo.config;
 
-import com.thoughtworks.spring.rbac.demo.filter.SSORequestHeaderAuthenticationFilter;
-import com.thoughtworks.spring.rbac.demo.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import com.thoughtworks.spring.rbac.demo.filter.AuthenticationFilter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
-
+@Component
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
-        builder.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-    }
+    public static final String SSO_URL = "http://localhost:8100/sso";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(ssoFilter(), RequestHeaderAuthenticationFilter.class)
-                .authenticationProvider(
-                        preauthAuthProvider())
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated();
+        http.
+                csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().
+                authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new W3LoginEntryPoint(SSO_URL));
+
+        http.addFilterBefore(new AuthenticationFilter(authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(preauthAuthProvider());
-    }
-
-    @Bean
-    public UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() {
-        UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper =
-                new UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken>();
-        wrapper.setUserDetailsService(new CustomUserDetailsService());
-        return wrapper;
-    }
-
-    @Bean
-    public PreAuthenticatedAuthenticationProvider preauthAuthProvider() {
-        PreAuthenticatedAuthenticationProvider preauthAuthProvider =
-                new PreAuthenticatedAuthenticationProvider();
-        preauthAuthProvider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
-        return preauthAuthProvider;
-    }
-
-    @Bean
-    public SSORequestHeaderAuthenticationFilter ssoFilter() throws Exception {
-        SSORequestHeaderAuthenticationFilter filter = new SSORequestHeaderAuthenticationFilter();
-        filter.setAuthenticationManager(authenticationManager());
-        return filter;
-    }
 }
--- src/main/java/com/thoughtworks/spring/rbac/demo/controller/ProtectedResourceController.java
+++ src/main/java/com/thoughtworks/spring/rbac/demo/controller/ProtectedResourceController.java
@@ -1,9 +1,6 @@
 package com.thoughtworks.spring.rbac.demo.controller;
 
 import com.thoughtworks.spring.rbac.demo.entity.Message;
-import com.thoughtworks.spring.rbac.demo.service.PermissionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/protected")
 public class ProtectedResourceController {
 
-    @Autowired
-    private PermissionService permissionService;
-
-    @PreAuthorize("@permissionService.check(principal, #id)")
     @RequestMapping("/{id}")
     public Message getOne(@PathVariable("id") String id) {
         return new Message("Protected resource");
